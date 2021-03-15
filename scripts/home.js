22e7a3e525fcfa030f19cfe5bed623bcdf8a9ae4
Santiago Vargas Vega <santiagovargasve@gmail.com>
2021-03-15 11:30:50 -0500

    Configurado ESlint y Prettier

--- scripts/home.js
+++ scripts/home.js
@@ -1,104 +1,102 @@
-const url = 'https://gist.githubusercontent.com/jhonatan89/719f8a95a8dce961597f04b3ce37f97b/raw/4b7f1ac723a14b372ba6899ce63dbd7c2679e345/products-ecommerce'
-let data 
+const url =
+  "https://gist.githubusercontent.com/jhonatan89/719f8a95a8dce961597f04b3ce37f97b/raw/4b7f1ac723a14b372ba6899ce63dbd7c2679e345/products-ecommerce";
+let data;
 
+const retrieveJson = async () => {
+  await fetch(
+    "https://gist.githubusercontent.com/jhonatan89/719f8a95a8dce961597f04b3ce37f97b/raw/4b7f1ac723a14b372ba6899ce63dbd7c2679e345/products-ecommerce",
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      data = json;
+    });
+};
 
-const retrieveJson = async() =>{
-    await fetch('https://gist.githubusercontent.com/jhonatan89/719f8a95a8dce961597f04b3ce37f97b/raw/4b7f1ac723a14b372ba6899ce63dbd7c2679e345/products-ecommerce')
-    .then(response => response.json())
-    .then(json => {
-        data = json
-    })
-    
-}
+const createList = async () => {
+  localStorage.setItem("hola", "mundo");
+  await retrieveJson();
+  let product = document.getElementsByClassName("product-container");
+  let container = document.getElementById("container");
 
+  data.items.forEach((element) => {
+    let title = element.title;
+    let pictureURL = element.picture;
+    let free_shipping = element.free_shipping;
+    let price = element.price.amount;
+    let id = element.id;
 
-const createList = async() =>{
-    localStorage.setItem('hola','mundo')
-    await retrieveJson()
-    let product =document.getElementsByClassName('product-container')
-    let container = document.getElementById('container')
+    let containerProduct = document.createElement("div");
+    let img = document.createElement("div");
+    let price_name = document.createElement("div");
+    let price_delivery = document.createElement("div");
+    let priceP = document.createElement("p");
+    let delivery = document.createElement("img");
+    let name = document.createElement("div");
+    let city = document.createElement("div");
+    containerProduct.className = "product-container";
+    containerProduct.id = id + "?";
+    img.className = "img-phone";
+    img.style.backgroundImage = `url('${pictureURL}')`;
+    img.id = id;
+    img.role = "button";
+    img.onclick = function (img) {
+      localStorage.removeItem("detail");
+      localStorage.setItem("detail", this.id);
+      window.location.href = "/detail.html";
+    };
+    containerProduct.appendChild(img);
 
-    data.items.forEach(element =>{
-
-        let title = element.title
-        let pictureURL = element.picture
-        let free_shipping = element.free_shipping
-        let price = element.price.amount
-        let id = element.id
-
-        let containerProduct = document.createElement('div')
-        let img =   document.createElement('div')
-        let price_name= document.createElement('div')
-        let price_delivery = document.createElement('div')
-        let priceP = document.createElement('p')
-        let delivery = document.createElement('img')
-        let name= document.createElement('div')
-        let city = document.createElement('div')
-        containerProduct.className = 'product-container'
-        containerProduct.id = id + '?'
-        img.className= 'img-phone'
-        img.style.backgroundImage=`url('${pictureURL}')`
-        img.id=id
-        img.role='button'
-        img.onclick = function (img){
-            localStorage.removeItem('detail')
-            localStorage.setItem('detail',this.id)
-            window.location.href='/detail.html'
-        }
-        containerProduct.appendChild(img)
-
-        price_name.className = 'price-name'
-        price_delivery.className = 'price-delivery'
-        priceP.className = 'price'
-        price= new Intl.NumberFormat('en-EN',{
-            style:'currency',
-            currency: 'USD'
-        }).format(price)
-        price = price.slice(0,-3).replace(/,/g,'.')
-        priceP.innerHTML=price
-        delivery.className='delivery'
-        if(free_shipping){
-            delivery.src='../assets/delivery.png'
-        }else{
-            delivery.src='../assets/white.png'
-        }
-        price_delivery.appendChild(priceP)
-        price_delivery.appendChild(delivery)
-        name.className = 'name'
-        name.innerHTML = title
-        price_name.appendChild(price_delivery)
-        price_name.appendChild(name)
-        containerProduct.appendChild(price_name)
-        city.className = 'city'
-        city.innerHTML = element.location
-        containerProduct.appendChild(city)
-
-
-        container.appendChild(containerProduct)
-    })
-
-    let search_button = document.getElementById('search-button')
-    search_button.onclick = getCategories
-}
-
-const getCategories = () =>{
-    let text = document.getElementById('search-area').value
-    let result = false
-    let containers = document.querySelectorAll('.product-container')
-    let items = data.items
-    containers.forEach(element =>{
-        let actid = element.id.replace('?','')
-        let isCategory = items.filter( item =>{
-            return item.categories.includes(text) && item.id == actid
-        })
-        if (isCategory.length == 0){
-            element.style.display = 'none'
-        }else {
-            result= true
-        }
-    })
-    if (!result){
-        alert('No se encontro ningun producto con esta categoria')
+    price_name.className = "price-name";
+    price_delivery.className = "price-delivery";
+    priceP.className = "price";
+    price = new Intl.NumberFormat("en-EN", {
+      style: "currency",
+      currency: "USD",
+    }).format(price);
+    price = price.slice(0, -3).replace(/,/g, ".");
+    priceP.innerHTML = price;
+    delivery.className = "delivery";
+    if (free_shipping) {
+      delivery.src = "../assets/delivery.png";
+    } else {
+      delivery.src = "../assets/white.png";
     }
-}
-createList()
+    price_delivery.appendChild(priceP);
+    price_delivery.appendChild(delivery);
+    name.className = "name";
+    name.innerHTML = title;
+    price_name.appendChild(price_delivery);
+    price_name.appendChild(name);
+    containerProduct.appendChild(price_name);
+    city.className = "city";
+    city.innerHTML = element.location;
+    containerProduct.appendChild(city);
+
+    container.appendChild(containerProduct);
+  });
+
+  let search_button = document.getElementById("search-button");
+  search_button.onclick = getCategories;
+};
+
+const getCategories = () => {
+  let text = document.getElementById("search-area").value;
+  let result = false;
+  let containers = document.querySelectorAll(".product-container");
+  let items = data.items;
+  containers.forEach((element) => {
+    let actid = element.id.replace("?", "");
+    let isCategory = items.filter((item) => {
+      return item.categories.includes(text) && item.id == actid;
+    });
+    if (isCategory.length == 0) {
+      element.style.display = "none";
+    } else {
+      result = true;
+    }
+  });
+  if (!result) {
+    alert("No se encontro ningun producto con esta categoria");
+  }
+};
+createList();
